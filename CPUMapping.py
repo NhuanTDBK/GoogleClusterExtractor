79a9140c79a51a0555d28e37b628f380eb46a469
Nhuan Tran <tranducnhuan1994@gmail.com>
2016-04-20 13:33:34 +0000

    Preview

--- CPUMapping.py
+++ CPUMapping.py
@@ -12,18 +12,17 @@ import numpy as np
 sc = SparkContext()
 sqlContext = SQLContext(sc)
 
-schema_path = '/home/nhuanhunter/MyWorking/SparkInvestor/data/schema_tu.csv'
-data_path = '/home/nhuanhunter/MyWorking/SparkInvestor/data/par0.csv'
-
+schema_path = 'schema_tu.csv'
+data_path = '/home/ubuntu/google_cluster_data/task_usage/task_usage/part-00499-of-00500.csv'
 schema = pd.read_csv(schema_path)
 dat = pd.read_csv(data_path,names=schema.columns)
-sample_dat = dat[["stime","etime","CPU_rate","mID"]]
+sample_dat = dat[["stime","etime","CPU_rate","mID"]][:3000]
 sample_rdd = sqlContext.createDataFrame(sample_dat)
-
 sample_rdd.registerTempTable("metrics")
 cpu_val = sqlContext.sql("SELECT * from metrics")
-mapper = cpu_val.flatMap(flatMap_moments).reduceByKey(reduce_moments).map(map_moments)
-t = mapper.toPandas().to_csv('/home/ubuntu/machine_usage.csv')
+mapper = cpu_val.flatMap(flatMap_moments).reduceByKey(reduce_moments).map(map_sorted).sortByKey(True,4).map(map_moments).collect()
+df = pd.DataFrame(mapper,columns=['mID','moment','CPU'])
+df.to_csv('mapperr',index=False)
 # <codecell>
-
+sc.stop()
 
--- MapFunction.py
+++ MapFunction.py
@@ -4,6 +4,8 @@ def flatMap_moments(T):
     stime = T[0]
     etime = T[1]
     moments = np.arange(stime,etime,step=1E6)
-    return [((T[2],t),T[3]) for t in moments]
-reduce_moments = lambda (x,y) : x[1]+y[1]
-map_moments = lambda (x) :(x[0][0],x[0][1],x[1])
+    return [((t,T[3]),T[2]) for t in moments]
+def reduce_moments(x,y):
+	return x+y
+map_sorted = lambda (x): (x[0][1],(x[0][0],x[1]))
+map_moments = lambda (x) :(x[0],x[1][0],x[1][1])
