a0f30c842dd72812d1094f24770527a6ae785e2f
Tran Duc Nhuan <tranducnhuan1994@gmail.com>
2016-04-21 22:37:18 +0700

    New Code

--- CPUMapping.py
+++ CPUMapping.py
@@ -7,22 +7,27 @@ from pyspark import SparkConf, SparkContext
 import pandas as pd
 from pyspark.sql import SQLContext
 from MapFunction import *
-
 import numpy as np
+import os
+
 sc = SparkContext()
 sqlContext = SQLContext(sc)
-
+folder_path ='/home/ubuntu/google_cluster_data/task_usage/task_usage/'
 schema_path = 'schema_tu.csv'
-data_path = '/home/ubuntu/google_cluster_data/task_usage/task_usage/part-00499-of-00500.csv'
+
+# data_path = '/home/ubuntu/google_cluster_data/task_usage/task_usage/part-00499-of-00500.csv'
 schema = pd.read_csv(schema_path)
-dat = pd.read_csv(data_path,names=schema.columns)
-sample_dat = dat[["stime","etime","CPU_rate","mID"]][:3000]
-sample_rdd = sqlContext.createDataFrame(sample_dat)
-sample_rdd.registerTempTable("metrics")
-cpu_val = sqlContext.sql("SELECT * from metrics")
-mapper = cpu_val.flatMap(flatMap_moments).reduceByKey(reduce_moments).map(map_sorted).sortByKey(True,4).map(map_moments).collect()
-df = pd.DataFrame(mapper,columns=['mID','moment','CPU'])
-df.to_csv('mapperr',index=False)
-# <codecell>
+topMID = pd.read_csv('top10MID.csv',header=None)
+topFile = pd.read_csv('topFile.csv',header=None)
+for file_name in topFile:
+    dat = pd.read_csv("%s%s"%(folder_path,file_name),names=schema.columns)
+    sample_dat = dat[["stime","etime","CPU_rate","mID"]]
+    sample_dat = sample_dat[sample_dat.mID.isin(topMID.mID)]
+    sample_rdd = sqlContext.createDataFrame(sample_dat)
+    sample_rdd.registerTempTable("metrics")
+    cpu_val = sqlContext.sql("SELECT * from metrics")
+    mapper = cpu_val.flatMap(flatMap_moments).reduceByKey(reduce_moments).map(map_sorted).sortByKey(True,4).map(map_moments).collect()
+    df = pd.DataFrame(mapper,columns=['mID','moment','CPU'])
+    df.to_csv('mapperr_%s'%file_name,index=False)
 sc.stop()
 
--- MapFunction.py
+++ MapFunction.py
@@ -9,3 +9,9 @@ def reduce_moments(x,y):
 	return x+y
 map_sorted = lambda (x): (x[0][1],(x[0][0],x[1]))
 map_moments = lambda (x) :(x[0],x[1][0],x[1][1])
+def retrieveData(sqlContext,sample_dat):
+    sample_rdd = sqlContext.createDataFrame(sample_dat)
+    sample_rdd.registerTempTable("metrics")
+    cpu_val = sqlContext.sql("SELECT * from metrics")
+    mapper = cpu_val.flatMap(flatMap_moments).reduceByKey(reduce_moments).map(map_sorted).sortByKey(True, 4).map(
+        map_moments).collect()
